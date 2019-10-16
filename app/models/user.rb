b7ac9fb95406695936eb0c4e7eef766983c20db7
suzuki-kaito <uminto532@gmail.com>
2019-10-17 01:46:14 +0900

    Empty commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable
+  has_many :tweets
+
+  validates :nickname, presence: true, length: { maximum: 6 }
+
+end
